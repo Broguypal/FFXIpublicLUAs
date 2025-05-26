9ad4c0bf47c06aacfd30a155eef6c2b34c29e63a
Broguypal <145296607+Broguypal@users.noreply.github.com>
2025-05-25 22:52:40 -0600

    Update PUP.lua

--- PUP.lua
+++ PUP.lua
@@ -72,7 +72,6 @@ send_command('bind numpad3 gs c ToggleDistance')
 send_command('bind f9 input /item "Remedy" <me>')
 send_command('bind f10 input /item "Panacea" <me>')
 send_command('bind f11 input /item "Holy Water" <me>')
-send_command('bind f12 input //lua l AutoPUP')
 
 --------- GEAR DEFINED ------------------
 
