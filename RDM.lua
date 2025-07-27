5a14d0ca87c1b766b9e5398a821679eaa7b051bd
Broguypal <145296607+Broguypal@users.noreply.github.com>
2025-07-26 22:57:19 -0600

    Add files via upload

--- RDM.lua
+++ RDM.lua
@@ -29,6 +29,8 @@ gearswap_box = function()
   str = str..' Enfeeble Mode:\\cs(0,153,51)   '..Enfeeble_Mode..'\\cr\n'
   str = str..' Weapon Lock:\\cs(128,128,128)   '..Lock_Mode..'\\cr\n'
   str = str..' Shihei Amount: '..shihei..'\n'
+  str = str..' \\cs(255,64,64)[FIR]\\cr   \\cs(0,255,0)[WND]\\cr   \\cs(180,0,255)[THD]\\cr\n'
+  str = str..' \\cs(128,255,255)[ICE]\\cr   \\cs(165,100,40)[STN]\\cr   \\cs(64,128,255)[WTR]\\cr\n'
     return str
 end
 
