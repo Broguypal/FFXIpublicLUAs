5744c0267870d708cc34f0569774ebf1e5b49664
Broguypal <145296607+Broguypal@users.noreply.github.com>
2025-08-02 22:43:18 -0600

    Add files via upload

--- RUN.lua
+++ RUN.lua
@@ -13,15 +13,19 @@
 ---- 		Mode / Textbox settings 		----
 tp_mode = 'Hybrid'
 spell_mode = 'Normal'
+auto_mode = 'Off'
 
 tp_modes = {'Hybrid','DPS','AoETank','SingleTank','MagicEva','MagicAettir'}
 spell_modes = {'Normal','SIR'}
+auto_modes = {'On','Off'}
 
 gearswap_box = function()
   str = '           \\cs(120,0,128)RUNE FENCER\\cr\n'
   str = str..' TP Mode:\\cs(255,150,100)   '..tp_mode..'\\cr\n'
   str = str..' Spell Mode:\\cs(255,150,100)   '..spell_mode..'\\cr\n'
-    return str
+  str = str..' Auto Runes:\\cs(255,150,100)   '..auto_mode..'\\cr\n'
+str = str..' crl: \\cs(255,255,255)[LGT]\\cr \\cs(255,64,64)[FIR]\\cr \\cs(0,255,0)[WND]\\cr \\cs(180,0,255)[THD]\\cr\n'
+str = str..' alt: \\cs(80,60,100)[DRK]\\cr \\cs(128,255,255)[ICE]\\cr \\cs(165,100,40)[STN]\\cr \\cs(64,128,255)[WTR]\\cr\n'    return str
 end
 
 gearswap_box_config = {pos={x=1320,y=550},padding=8,text={font='sans-serif',size=10,stroke={width=2,alpha=255},Fonts={'sans-serif'},},bg={alpha=0},flags={}}
@@ -34,23 +38,35 @@ end
 
 function get_sets()
 ----				KEYBINDS			----
---Equipset toggles
-send_command('bind numpad9 gs c ToggleHybrid')
-send_command('bind numpad8 gs c ToggleTank')
-send_command('bind numpad7 gs c ToggleMagic')
+	--Equipset toggles
+	send_command('bind numpad9 gs c ToggleHybrid')
+	send_command('bind numpad8 gs c ToggleTank')
+	send_command('bind numpad7 gs c ToggleMagic')
 
--- Spell interupt toggle
-send_command('bind numpad6 gs c ToggleSIR')
+	-- Spell interupt toggle
+	send_command('bind numpad6 gs c ToggleSIR')
 
---weapon toggles
-send_command('bind numpad4 gs c ToggleWeapon')
+	-- Auto Runes toggle
+	send_command('bind numpad0 gs c ToggleAUTO')
 
---QOL commands
-send_command ('bind numpad1 input /mount "Noble Chocobo"')
-send_command ('bind numpad2 input /dismount')
-send_command('bind f9 input /item "Remedy" <me>')
-send_command('bind f10 input /item "Panacea" <me>')
-send_command('bind f11 input /item "Holy Water" <me>')
+	send_command ('bind ^numpad1 input /ja "Ignis" <me>')
+	send_command ('bind ^numpad2 input /ja "Flabra" <me>')
+	send_command ('bind ^numpad3 input /ja "Sulpor" <me>')
+	send_command ('bind ^numpad0 input /ja "Lux" <me>')
+	send_command ('bind !numpad1 input /ja "Gelus" <me>')
+	send_command ('bind !numpad2 input /ja "Tellus" <me>')
+	send_command ('bind !numpad3 input /ja "Unda" <me>')
+	send_command ('bind !numpad0 input /ja "Tenebrae" <me>')
+
+	--weapon toggles
+	send_command('bind numpad4 gs c ToggleWeapon')
+
+	--QOL commands
+	send_command ('bind numpad1 input /mount "Noble Chocobo"')
+	send_command ('bind numpad2 input /dismount')
+	send_command('bind f9 input /item "Remedy" <me>')
+	send_command('bind f10 input /item "Panacea" <me>')
+	send_command('bind f11 input /item "Holy Water" <me>')
 
 --- 			EQUIPMENT SETS			----
 
@@ -592,6 +608,50 @@ function aftercast(spell)
 	end
 end
 
+-- Auto RUNE Logic
+windower.register_event('lose buff', function(buff_id)
+	if buff_id == 523 then
+		if auto_mode == "On" and player.hp > 0 then
+			send_command('input /ja "Ignis" <me>')
+		end
+	end
+	if buff_id == 524 then
+		if auto_mode == "On" and player.hp > 0 then
+			send_command('input /ja "Gelus" <me>')
+		end
+	end
+	if buff_id == 525 then
+		if auto_mode == "On" and player.hp > 0 and pet and pet.isvalid then
+			send_command('input /ja "Flabra" <me>')
+		end
+	end
+	if buff_id == 526 then
+		if auto_mode == "On" and player.hp > 0 then
+			send_command('input /ja "Tellus" <me>')
+		end
+	end
+	if buff_id == 527 then
+		if auto_mode == "On" and player.hp > 0 then
+			send_command('input /ja "Sulpor" <me>')
+		end
+	end
+	if buff_id == 528 then
+		if auto_mode== "On" and player.hp > 0 then
+			send_command('input /ja "Unda" <me>')
+		end
+	end
+	if buff_id == 529 then
+		if auto_mode == "On" and player.hp > 0 then
+			send_command('input /ja "Lux" <me>')
+		end
+	end
+	if buff_id == 530 then
+		if auto_mode == "On" and player.hp > 0 then
+			send_command('input /ja "Tenebrae" <me>')
+		end
+	end
+end)
+
 -- rules for setting modes
 function self_command(command)
 	if command == "ToggleHybrid" then
@@ -640,6 +700,12 @@ function self_command(command)
         if next_weapon then
             send_command('input /equip Main "' .. next_weapon .. '"')
         end
+	elseif command == "ToggleAUTO" then
+		if auto_mode == "Off" then
+			auto_mode = "On"
+		else
+			auto_mode = "Off"
+		end
 	end
 	gearswap_jobbox:text(gearswap_box())		
 	gearswap_jobbox:show()
@@ -656,6 +722,7 @@ function file_unload()
 	send_command('unbind numpad3')
 	send_command('unbind numpad2')
 	send_command('unbind numpad1')
+	send_command('unbind numpad0')
 	send_command('unbind ^numpad9')
 	send_command('unbind ^numpad8')
 	send_command('unbind ^numpad7')
